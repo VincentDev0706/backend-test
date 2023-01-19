472e814c92eaf1c8b6be6484bab08abe1643d7a2
VincentDev0706 <122772475+VincentDev0706@users.noreply.github.com>
2023-01-18 20:45:10 -0500

    initial commit

--- index.js
+++ index.js
@@ -1,73 +1,65 @@
 const express = require("express");
 const { Worker } = require("worker_threads");
+const redis = require('redis');
+const REDIS_PORT = process.env.REDIS_PORT || 6379;
+const client = redis.createClient({
+  url: 'redis://redis:6379',
+  legacyMode: true,
+  });
+(async () => {
+    await client.connect();
+})();
+  
+console.log("Connecting to the Redis");
+  
+client.on("ready", () => {
+    console.log("Connected!");
+});
+  
+client.on("error", (err) => {
+    console.log("Error in the Connection");
+});
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get("/non-blocking/", (req, res) => {
-  res.status(200).send("This page is non-blocking");
-});
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-function calculateCount() {
-  return new Promise((resolve, reject) => {
-    let counter = 0;
-    for (let i = 0; i < 20_000_000_000; i++) {
-      counter++;
-    }
-    resolve(counter);
+let ticket_id = 1;
+app.post("/input", async (req, res) => {
+  const number = +req.body.number - 1;
+  const worker = new Worker("./worker.js", {
+    workerData: { num: number },
   });
-}
-
-//
-// promises don’t make JavaScript code execute in parallel
-// and cannot be used to make CPU-bound tasks non-blocking
-//
-// app.get("/blocking", async (req, res) => {
-//   const counter = await calculateCount();
-//   res.status(200).send(`result is ${counter}`);
-// });
-
-app.get("/blocking", async (req, res) => {
-  const worker = new Worker("./worker.js");
-  worker.on("message", (data) => {
-    res.status(200).send(`result is ${data}`);
+  worker.on("message", async (data) => {
+    // console.log(client.connect);
+    await client.set(ticket_id.toString(), data , function(err,reply) {
+      if(!err) {
+        console.log(reply);
+        res.status(200).send({'ticket':ticket_id});
+        ticket_id++;
+      }
+    });
+    
   });
 
   worker.on("error", (data) => {
-    res.status(404).send(`An error occurred: ${msg}`);
+    res.status(404).send(`An error occurred: ${data}`);
   });
 });
 
-const THREAD_COUNT = 4;
-
-function createWorker() {
-  return new Promise((resolve, reject) => {
-    const worker = new Worker("./four_workers.js", {
-      workerData: { thread_count: THREAD_COUNT },
-    });
-    worker.on("message", (data) => {
-      resolve(data);
-    });
-
-    worker.on("error", (data) => {
-      reject(`An error ocurred: ${msg}`);
-    });
-  });
-}
-
-app.get("/blocking-threads", async (req, res) => {
-  const workerPromises = [];
-  for (let i = 0; i < THREAD_COUNT; i++) {
-    workerPromises.push(createWorker());
-  }
-
-  const thread_results = await Promise.all(workerPromises);
-  const total =
-    thread_results[0] +
-    thread_results[1] +
-    thread_results[2] +
-    thread_results[3];
-  res.status(200).send(`result is ${total}`);
+app.get("/output", async (req, res) => {
+  const ticket = req.query.ticket;  
+  console.log(ticket);
+  await client.get(ticket.toString(), function(err, reply) {
+    console.log(reply);
+    if(reply == null) {
+      res.status(200).send({'answer':'not found'});
+    } else {
+      res.status(200).send({'Fibonacci':reply});
+    }
+  });       
 });
 
 app.listen(port, () => {
